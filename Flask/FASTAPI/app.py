6adc69800236ddc89785eefbc5ebd9c4d31bc307
Harsh Sisodiya <harshsisodiya678@Harshs-MacBook-Pro.local>
2026-08-17 18:59:10 +0800

    FASTAPI

---
+++ Flask/FASTAPI/app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+# FastAPI Object
+app = FastAPI()
+
+# Load Trained Model
+model = joblib.load("salary_model.joblib")
+
+# Input Schema
+class Employee(BaseModel):
+    experience: float
+
+# Home Route
+@app.get("/")
+def home():
+    return {
+        "message": "Salary Prediction API Running abc"
+    }   
+
+# Prediction Route
+@app.post("/predict")
+def predict_salary(data: Employee):
+
+    exp = np.array([[data.experience]])
+
+    prediction = model.predict(exp)
+
+    return {
+        "experience": data.experience,
+        "predicted_salary": float(prediction[0])
+    }
