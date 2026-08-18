d20dae22e674d0b38ae31366c0ff71b116b9534a
sisodiaharsh678-cmd <sisodiaharsh678@gmail.com>
2026-08-18 21:14:41 +0800

    Update salary predictor app.py

--- Flask/FASTAPI/app.py
+++ Flask/FASTAPI/app.py
@@ -17,7 +17,7 @@ class Employee(BaseModel):
 @app.get("/")
 def home():
     return {
-        "message": "Salary Prediction API Running abc and xyz"
+        "message": "Salary Prediction API Running abc and welcome"
     }   
 
 # Prediction Route
